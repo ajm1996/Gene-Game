2407ed11d7ced661eb445e2198be2e652d76e0ec
Asher Summers <55416438+ashersummers@users.noreply.github.com>
2024-01-22 22:09:08 -0700

    Breeding menu scroll view
    Scroll view now properly populates with living allies in a grid formation.

--- Assets/Resources/Scripts/BreedingMenu.cs
+++ Assets/Resources/Scripts/BreedingMenu.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class BreedingMenu : MonoBehaviour
 {
+
+    public GameObject scrollMenuContent;
+    public GameObject droneImage;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Get list of living ally drones and populate scroll view with it
+        List<GameObject> livingAllies = GameObject.Find("Main Camera").GetComponent<Game>().livingAllies;
+        SetupDroneScrollView(livingAllies);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void Breed() {
+        Debug.Log("Clicked breed");
+    }
+
+    void SetupDroneScrollView(List<GameObject> livingAllies) {
+        for (var i = 0; i < livingAllies.Count; i++) {
+            AddDroneToScrollView();
+        }
+    }
+
+    void AddDroneToScrollView() {
+        GameObject drone = Instantiate(droneImage);
+        drone.transform.SetParent(scrollMenuContent.transform, false);
     }
 }
