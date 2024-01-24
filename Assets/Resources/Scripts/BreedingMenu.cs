8f00c408bd3605f7cdd7e6cfde3fe79eb8837c37
zackaljackal <51419870+zackaljackal@users.noreply.github.com>
2024-01-24 00:42:03 -0500

    Added Traits List and Fix UI Scaling
    Added a Traits list that will fill out/empty when a drone is selected/deselected. Also fixed UI scaling within the breeding menus so it will always follow a ratio of the current resolution

--- Assets/Resources/Scripts/BreedingMenu.cs
+++ Assets/Resources/Scripts/BreedingMenu.cs
@@ -1,6 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
+using UnityEngine.UI;
 
 public class BreedingMenu : MonoBehaviour
 {
@@ -14,6 +18,9 @@ public class BreedingMenu : MonoBehaviour
     private bool selectTwoSelecting;
     public GameObject firstSelectedDrone;
     public GameObject secondSelectedDrone;
+    public GameObject droneOneTraitScrollMenuContent;
+    public GameObject droneTwoTraitScrollMenuContent;
+    public GameObject traitText;
 
     // Start is called before the first frame update
     void Start()
@@ -100,22 +107,46 @@ public class BreedingMenu : MonoBehaviour
     void AddDroneToSelectOne(GameObject allyDrone) {
         firstSelectedDrone = allyDrone;
         allyDrone.transform.SetParent(selectOneContent.transform, false);
+        List<Trait> droneOneTraits = allyDrone.GetComponent<DroneImage>().linkedDrone.GetComponent<AllyDrone>().GetTraits();
+        for (int i = 0; i < droneOneTraits.Count; i++)
+        {
+            var newTraitText = Instantiate(traitText);
+            newTraitText.GetComponent<TextMeshProUGUI>().text = droneOneTraits[i].Id.ToString() + " - " + "Short";
+            newTraitText.transform.SetParent(droneOneTraitScrollMenuContent.transform, false);
+        }
     }
 
     void AddDroneToSelectTwo(GameObject allyDrone) {
         secondSelectedDrone = allyDrone;
         allyDrone.transform.SetParent(selectTwoContent.transform, false);
+        List<Trait> droneTwoTraits = allyDrone.GetComponent<DroneImage>().linkedDrone.GetComponent<AllyDrone>().GetTraits();
+        for (int i = 0; i < droneTwoTraits.Count; i++)
+        {
+            var newTraitText = Instantiate(traitText);
+            newTraitText.GetComponent<TextMeshProUGUI>().text = droneTwoTraits[i].Id.ToString() + " - " + "Description Here With Really Long Text Description";
+            newTraitText.transform.SetParent(droneTwoTraitScrollMenuContent.transform, false);
+        }
     }
 
     void RemoveDroneFromSelectOne() {
         GameObject droneToRemove = firstSelectedDrone;
         droneToRemove.transform.SetParent(scrollMenuContent.transform, false);
+        var droneOneTraitsList = droneOneTraitScrollMenuContent.GetComponentsInChildren<TextMeshProUGUI>();
+        for (int i = 0; i < droneOneTraitsList.Length; i++)
+        {
+            Destroy(droneOneTraitsList[i].gameObject);
+        }
         firstSelectedDrone = null;
     }
 
     void RemoveDroneFromSelectTwo() {
         GameObject droneToRemove = secondSelectedDrone;
         droneToRemove.transform.SetParent(scrollMenuContent.transform, false);
+        var droneTwoTraitsList = droneTwoTraitScrollMenuContent.GetComponentsInChildren<TextMeshProUGUI>();
+        for (int i = 0; i < droneTwoTraitsList.Length; i++)
+        {
+            Destroy(droneTwoTraitsList[i].gameObject);
+        }
         secondSelectedDrone = null;
     }
 }
